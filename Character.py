1225ffe12ff5eba139c394d101002ebcba48d380
mgmacias95 <mgmacias95@correo.ugr.es>
2016-10-22 18:16:18 +0200

    POR  FIIIIIINNNNNNNNNN SE VE EL PERSONAJE SJBJRBJTRBHRBLRE

--- Character.py
+++ Character.py
@@ -28,39 +28,32 @@ class Character(Physics):
     def __init__(self, hp, position, sprite):
         super(Character, self).__init__(img_path=sprite, position = position)
         self.hp = hp
-        self.abajo, self.arriba, self.dcha, self.izq = self._cortar_chara(fil=3)
         self.rect = self.image.get_rect()
+        self.abajo, self.arriba, self.dcha, self.izq = self._cortar_chara(fil=3)
+        self.img = self.image.subsurface((self.abajo[0][0], self.abajo[0][1], 32, 32))
 
-    # Corta un chara en las fil y col indicadas. Array Bidimensional.
-    # tomada de: http://razonartificial.com/2010/06/engine-xi-creando-al-heroe/
+    # Corta un chara en las fil y col indicadas.
     def _cortar_chara(self, fil):
         # La idea de esta función es devolver una tupla con cuatro vectores:
         #       * sprites de movimiento hacia la izquierda
         #       * sprites de movimiento hacia la derecha
         #       * sprites de movimiento hacia arriba
         #       * sprites de movimiento hacia abajo
-        # HELP T_T
         abajo = [0]*fil
         arriba = [0]*fil
         dcha = [0]*fil
         izq = [0]*fil
 
         for i in range(fil):
-            # self.abajo[i] = self.image[i*32:(i+1)*32,i*32:(i+1)*32]
-            # self.izq[i] = self.image[i * 32:(i + 1) * 32, (i+1) * 32:(i + 2) * 32]
-            # self.dcha[i] = self.image[i * 32:(i + 1) * 32, (i + 2) * 32:(i + 3) * 32]
-            # self.arriba[i] = self.image[i * 32:(i + 1) * 32, (i + 3) * 32:(i + 4) * 32]
-            abajo[i] = self.rect[i]
-            # izq[i] = self.rect[i+1]
-            # dcha[i] = self.rect[i+2]
-            # arriba[i] = self.rect[i+3]
-
-        self.image = self.abajo[0]
+            abajo[i]  = (0,  i*32)
+            izq[i]    = (32, i*32)
+            dcha[i]   = (64, i*32)
+            arriba[i] = (96, i*32)
 
         return (abajo, arriba, dcha, izq)
 
     def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        screen.blit(self.img, self.rect)
 
     def attack(self):
         raise NotImplemented("Implement the atack in MainCharacter and Enemy")
